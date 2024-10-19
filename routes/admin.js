eb25f5c26d229c327720bc856348e2ab85957302
chakrapani <chakrapani24@gmail.com>
2024-10-19 15:33:02 +0530

    bycrypt updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../models/Admin');
+const bcrypt = require('bcryptjs');
 
 // Create a new admin
 router.post('/', async (req, res, next) => {
