abb736298712616afad1196f00135cd910cf94b1
Chakrapani Upadhyaya <56042073+uchakrapani@users.noreply.github.com>
2024-10-21 10:40:27 +0530

    Create Contact.js

---
+++ models/Contact.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, match: /.+\@.+\..+/ },
+  phone: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
